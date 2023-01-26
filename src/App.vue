e089e2bad56ca1ecdb36fef51988ae7b2a273b2d
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-26 15:35:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {};
+</script>
+<template>
+  <div class="container">
+    <h1>ciao vue, chi non muore se rivede</h1>
+  </div>
+</template>
+
+
+<style lang="scss">
+@use "./style/general";
+</style>
