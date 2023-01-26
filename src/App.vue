cc073e19401c000303eb4e58f3aa14a6a191b3ff
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-26 17:50:53 +0100

    stampa in card

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import axios from "axios";
+import ProjectCard from "./components/ProjectCard.vue";
 
 export default {
+  components: { ProjectCard },
   name: "App",
   data() {
     return {
-      baseURL: "http://localhost:8000/api",
+      baseURL: "http://127.0.0.1:8000/api",
       projectURI: "/project",
       projects: [],
     };
@@ -13,7 +15,7 @@ export default {
   methods: {
     getApi() {
       axios.get(this.baseURL + this.projectURI).then((r) => {
-        this.projects = r.data;
+        this.projects = r.data.projects;
         console.log(this.projects);
       });
       //  .catch(error=>(errorMsg=error))
@@ -26,7 +28,18 @@ export default {
 </script>
 <template>
   <div class="container">
-    <h1>ciao vue, chi non muore se rivede!</h1>
+    <div class="row">
+      <div class="col-12">
+        <h1>ciao vue, chi non muore se rivede!</h1>
+      </div>
+      <div class="col">
+        <ProjectCard
+          v-for="project in projects"
+          :key="project.id"
+          :project="project"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "ProjectCard",
+  data() {
+    return {};
+  },
+  props: {
+    project: Object,
+  },
+};
+</script>
+<template>
+  <div class="card" style="width: 18rem">
+    <div class="card-body">
+      <h5 class="card-title">{{ project.name }}</h5>
+      <p class="card-text">
+        {{ project.summary }}
+      </p>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+</style>
