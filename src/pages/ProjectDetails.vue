b2afdfe1915457649664cc936e90a73da252f9a0
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-27 18:26:29 +0100

    stampa show

--- src/pages/ProjectDetails.vue
+++ src/pages/ProjectDetails.vue
@@ -1,10 +1,44 @@
 <script>
+import axios from "axios";
+
 export default {
   name: "ProjectDetails",
+  data() {
+    return {
+      baseURL: "http://127.0.0.1:8000/api",
+      projectURI: "/project/",
+
+      project: {},
+    };
+  },
+  methods: {
+    getApi2() {
+      console.log(this.baseURL + this.projectURI + this.$route.params.slug);
+      axios
+        .get(this.baseURL + this.projectURI + this.$route.params.slug)
+        .then((r) => {
+          this.project = r.data;
+          // console.log(this.projects);
+        });
+      //  .catch(error=>(errorMsg=error))
+    },
+  },
+  created() {
+    this.getApi2();
+  },
 };
 </script>
 <template>
-  <h1>project detail</h1>
+  <div class="card" style="width: 18rem">
+    <img src="..." class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">
+        <h1>{{ project.name }}</h1>
+      </h5>
+      <p class="card-text">{{ project.summary }}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
 </template>
 
 
