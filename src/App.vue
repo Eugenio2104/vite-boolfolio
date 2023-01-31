7c90341f1a4af8681d034e925859e0fcf19fc8c5
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-31 15:41:00 +0100

    inserimento form mail

--- src/App.vue
+++ src/App.vue
@@ -28,8 +28,27 @@ export default {
 };
 </script>
 <template>
-  <Header class="mb-5" />
-  <router-view> </router-view>
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.css"
+        integrity="sha512-FA9cIbtlP61W0PRtX36P6CGRy0vZs0C2Uw26Q1cMmj3xwhftftymr0sj8/YeezDnRwL9wtWw8ZwtCiTDXlXGjQ=="
+        crossorigin="anonymous"
+      />
+      <title>Document</title>
+    </head>
+    <body></body>
+  </html>
+
+  <body>
+    <Header class="mb-5" />
+    <router-view> </router-view>
+  </body>
 </template>
 
 
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,10 +1,16 @@
+
+
 <script>
+import ContactForm from "../components/ContactForm.vue";
 export default {
-  name: "Home",
+  components: { ContactForm },
+  name: "Contacts",
 };
 </script>
+
 <template>
   <h1>contatti</h1>
+  <ContactForm />
 </template>
 
 
